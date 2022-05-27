032fa42d2ff38d61775e9bd84c9c73e31478761b
claremburu <mburuclare8@gmail.com>
2022-05-27 04:47:25 -0700

    created redux store

--- store/local/environment.js
+++
@@ -1,5 +0,0 @@
-const swaggerApi = "https://18.168.211.166:30084/"
-
-const Environment = {
-    apiUrl: swaggerApi
-}
--- store/store.js
+++
@@ -1,7 +0,0 @@
-// import { configureStore } from 'redux';
-// import {authReducer} from './reducers/authReducer'
-
-// // ...creating a store
-// const store = configureStore(authReducer)
-
-// export default store
